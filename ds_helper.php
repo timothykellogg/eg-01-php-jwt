a363ad5ac468d5dc74992da7b3871d9da4ee07ab
Shadi Massalha <shady.massalha@gmail.com>
2018-06-24 15:37:40 +0300

    - add print pretty JSON - read file content

--- ds_helper.php
+++ ds_helper.php
@@ -1,1 +1,23 @@
-<?php ?>
+<?php
+
+class DSHelper {
+
+    public static function readContent($file_name) {
+        $filename = $file_name;
+        $handle = fopen($filename, "rb");
+        $fsize = filesize($filename);
+        $contents = fread($handle, $fsize);
+        // $byteArray = unpack("N*",$contents);
+        // $str=implode(array_map("chr", $cont));
+        print($str);
+        return $contents;
+    }
+
+    public static function printPrettyJSON($json) {
+        $pp = json_encode($json, JSON_PRETTY_PRINT);
+        $pp = str_replace ( '\/' , '/' , $pp );
+        printf ("%s\n", $pp);
+    }
+}
+
+?>
