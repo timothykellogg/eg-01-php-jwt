ecde24720fcf5a38061f15c60d755afbd88b4de5
Shadi Massalha <shady.massalha@gmail.com>
2018-06-25 11:34:47 +0300

    add other functionality

--- main.php
+++ main.php
@@ -4,6 +4,9 @@
     include_once 'example_base.php';
     include_once 'send_envelope.php';
     include_once 'list_envelopes.php';
+    include_once 'get_envelope_status.php';
+    include_once 'list_envelope_recipients.php';
+    include_once 'get_envelope_documents.php';
 
     new DSConfig();
 
@@ -21,5 +24,33 @@
     $listEnvelopesHandler= new ListEnvelopes($apiClient);
     $envelopesList = $listEnvelopesHandler->list();
 
+    $envelopes = $envelopesList->getEnvelopes();
+
+    if(!is_null($envelopesList)  && count($envelopes) > 2) {
+        printf("\nResults for %d envelopes were returned. Showing the first two:", count($envelopes));
+        $envelopesList->setEnvelopes(array($envelopes[0],$envelopes[1]));
+    }
+
+    $firstEnvelopeId;
+    // Save an envelopeId for later use if an envelope list was returned (result set could be empty)
+    if(!is_null($envelopes) && !is_null($envelopes[0])){
+        $firstEnvelopeId = $envelopes[0]->getEnvelopeId();
+    }
+
+    DSHelper::printPrettyJSON($envelopesList);
+
+    print("\nGet an envelope's status...");
+    $getEnvelopeStatusHandler = new GetEnvelopeStatus($apiClient);
+    $envelope = $getEnvelopeStatusHandler->getEnvelope($firstEnvelopeId);
+    DSHelper::printPrettyJSON($envelope);
+
+    print("\nList an envelope's recipients and their status...");
+    $ListEnvelopeRecipientsHandler = new ListEnvelopeRecipients($apiClient);
+    $recipients = $ListEnvelopeRecipientsHandler->list($firstEnvelopeId);
+    DSHelper::printPrettyJSON($recipients);
+
+    print("\nDownload an envelope's document(s)...");
+    $getEnvelopeDocumentsHandler = new GetEnvelopeDocuments($apiClient);
+    $getEnvelopeDocumentsHandler->download($firstEnvelopeId);
 
     ?>
