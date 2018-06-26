cb59105d08436b90be5322a748306edf83a9ab7d
Shadi Massalha <shady.massalha@gmail.com>
2018-06-26 10:56:49 +0300

    move all examples logic to lib folder

---
+++ lib/list_envelopes.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('vendor/docusign/esign-client/autoload.php');
+    include_once 'example_base.php';
+    /**
+     *
+     */
+    class ListEnvelopes extends ExampleBase {
+        public function __construct($client) {
+            parent::__construct($client);
+        }
+        /**
+         *
+         */
+        public function list(){
+            $this->validateToken();
+            $envelopeApi = new DocuSign\eSign\Api\EnvelopesApi(self::$apiClient);
+
+            $options = new DocuSign\eSign\Api\EnvelopesApi\ListStatusChangesOptions();
+            $date = new Datetime();
+            $date->sub(new DateInterval("P30D"));
+            $options->setFromDate($date->format("Y/m/d"));
+            return $envelopeApi->listStatusChanges(self::$accountID, $options);
+        }
+    }
+?>
