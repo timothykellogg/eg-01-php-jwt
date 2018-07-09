bf14ae4ec2f45e5df048c650e1f15c2e23b6c7bd
Shadi Massalha <shady.massalha@gmail.com>
2018-07-09 16:19:53 +0300

    refactor example logic

--- main.php
+++ main.php
@@ -20,35 +20,7 @@
 
     print("\nList envelopes in the account...");
     $listEnvelopesHandler= new ListEnvelopes($apiClient);
-    $envelopesList = $listEnvelopesHandler->list();
+    $listEnvelopesHandler->list();
 
-    $envelopes = $envelopesList->getEnvelopes();
-
-    if(!is_null($envelopesList)  && count($envelopes) > 2) {
-        printf("\nResults for %d envelopes were returned. Showing the first two:", count($envelopes));
-        $envelopesList->setEnvelopes(array($envelopes[0],$envelopes[1]));
-    }
-
-    $firstEnvelopeId;
-    // Save an envelopeId for later use if an envelope list was returned (result set could be empty)
-    if(!is_null($envelopes) && !is_null($envelopes[0])){
-        $firstEnvelopeId = $envelopes[0]->getEnvelopeId();
-    }
-
-    DSHelper::printPrettyJSON($envelopesList);
-
-    print("\nGet an envelope's status...");
-    $getEnvelopeStatusHandler = new GetEnvelopeStatus($apiClient);
-    $envelope = $getEnvelopeStatusHandler->getEnvelope($firstEnvelopeId);
-    DSHelper::printPrettyJSON($envelope);
-
-    print("\nList an envelope's recipients and their status...");
-    $ListEnvelopeRecipientsHandler = new ListEnvelopeRecipients($apiClient);
-    $recipients = $ListEnvelopeRecipientsHandler->list($firstEnvelopeId);
-    DSHelper::printPrettyJSON($recipients);
-
-    print("\nDownload an envelope's document(s)...");
-    $getEnvelopeDocumentsHandler = new GetEnvelopeDocuments($apiClient);
-    $getEnvelopeDocumentsHandler->download($firstEnvelopeId);
-
+    print("\nDone.")
     ?>
